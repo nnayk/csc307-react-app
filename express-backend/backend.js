9f27510fd2565970b98e7af4ee15b40aff0f2a36
nnayk <79224511+nnayk@users.noreply.github.com>
2023-09-30 21:53:04 -0700

    Add users route. Currently using hardcoded users list

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -1,12 +1,46 @@
 import express from "express";
 
+const USERS = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ a: "Hello World!" });
+});
+
+app.get("/users", (req, res) => {
+  res.send(USERS);
 });
 
 app.listen(port, () => {
